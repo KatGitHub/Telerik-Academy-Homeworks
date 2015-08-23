7dc5630fc53e621358a643dae7626c578e4bc812
Ekaterina Martynyuk <martynyuk88@gmail.com>
2015-08-23 15:02:55 +0300

    Added solutions for Development Tools Homework

---
+++ High quality code/Development Tools Homework/Development-Tools-Homework/log4net/Log4Net.cs
@@ -0,0 +1,27 @@
+﻿namespace log4net
+{
+    using log4net.Config;
+    using System;
+
+    public static class Log4Net
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4Net));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Info("Info logging");
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception e)
+            {
+                Log.Error("This is my error", e);
+            }
+
+            Console.WriteLine("[any key to exit]");
+            Console.ReadKey();
+        }
+    }
+}
